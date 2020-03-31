6b2563ed0fe48703990d0a06fb588fe4c4b981d8
Angel Barusta <angel.barusta.95@gmail.com>
2020-03-30 20:35:00 -0600

    jsx

---
+++ react/src/JS/index.js
@@ -0,0 +1,26 @@
+import "../CSS/index.css";
+// import text from "./text.js";
+import search from "./search.js";
+import render from "./render.js";
+
+// console.log("HOLA PUTO HOME PLUGIN");
+
+// text();
+
+// if (module.hot) {
+//   module.hot.accept("./text.js", () => {
+//     // console.log("he recargado en caliente");
+//     text();
+//   });
+// }
+
+const id = prompt("Quien es ese pokemon?");
+
+search(id)
+  .then((data) => {
+    render(data);
+  })
+  .catch((err) => {
+    const error = new Error(`ERROR: ${err}`);
+    console.error(error);
+  });
