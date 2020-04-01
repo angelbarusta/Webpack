70708c9091f9f497afe29f8c21f3f06948389d0c
Angel Barusta <angel.barusta.95@gmail.com>
2020-04-01 15:48:23 -0600

    prevent

---
+++ prevent-duplication/src/JS/components/App.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import Loader from "./loader";
+
+import logo from "../../images/platzi.png";
+import video from "../../video/que-es-core.mp4";
+
+import "../../sass/index.scss";
+import "../../less/index.less";
+import "../../stylus/index.styl";
+
+const data = {
+  loaders: [
+    {
+      name: "css-loader",
+      id: "1"
+    },
+    {
+      name: "style-loader",
+      id: "2"
+    },
+    {
+      name: "babel-loader",
+      id: "3"
+    }
+  ]
+};
+
+const App = () => {
+  const [loaderList, setLoaderList] = useState([]);
+  const handleClick = () => {
+    setLoaderList(data);
+  };
+  const handleClickOcultar = () => {
+    setLoaderList([]);
+  };
+
+  return (
+    <div>
+      MI APP REACT
+      <p className='less'>esto es less</p>
+      <p className='sass'>esto es sass</p>
+      <p className='stylus'>esto es stylus</p>
+      <p className='post-css'>esto es postcss</p>
+      <video src={video} width={360} controls poster={logo}></video>
+      <p>
+        <img src={logo} alt='logo' width={40} />
+      </p>
+      <ul>
+        {loaderList.map((item) => (
+          <Loader {...item} key={item.id} />
+        ))}
+      </ul>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <button onClick={handleClick}>Mostrar lo aprendeido</button>
+        <button onClick={handleClickOcultar}>Ocultar lo aprendeido</button>
+      </div>
+    </div>
+  );
+};
+export default App;
