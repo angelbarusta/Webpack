cc697f12beb18cbd19ea82cf33f1f20516d76893
Angel Barusta <angel.barusta.95@gmail.com>
2020-03-31 20:20:19 -0600

    hook

--- react-hook/src/JS/components/App.js
+++ react-hook/src/JS/components/App.js
@@ -1,11 +1,30 @@
-import React from "react";
-import data from './data.json';
+import React, { useState } from "react";
+import data from "./data.json";
+import Loader from "./loader";
 
-// console.log(data);
+console.log(data);
 
 const App = () => {
-    return(
-        <div>MI APP REACT</div>
-    )
+  const [loaderList, setLoaderList] = useState([]);
+  const handleClick = () => {
+    setLoaderList(data.loaders);
+  };
+  const handleClickOcultar = () => {
+    setLoaderList([]);
+  };
+  return (
+    <div>
+      MI APP REACT
+      <ul>
+        {loaderList.map((item) => (
+          <Loader {...item} key={item.id} />
+        ))}
+      </ul>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <button onClick={handleClick}>Mostrar lo aprendeido</button>
+        <button onClick={handleClickOcultar}>Ocultar lo aprendeido</button>
+      </div>
+    </div>
+  );
 };
 export default App;
